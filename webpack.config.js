f0a130f10265667194c8bc0ff325094702e2c9a3
Elliot Chong <code@elliotjameschong.com>
2016-01-17 15:30:46 -0800

    chore: Big bang!

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+
+module.exports = {
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, '/build'),
+		filename: 'b2w.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
+}
