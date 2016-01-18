b8877bdfaeadb8d2388b7f6a68af53aedb0ef954
Craig Campbell <craigaaroncampbell@gmail.com>
2016-01-18 13:50:31 -0800

    touch star wins game

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-var game = new Phaser.Game(800, 800, Phaser.AUTO, '',{
+var game = new Phaser.Game(800, 750, Phaser.AUTO, '',{
   preload: preload, create: create, update, update
 });
 
 function preload(){
   game.load.image('ground', 'assets/sprites/platform.png')
-  game.load.image('ball', 'assets/sprites/orb-blue.png', 50, 50)
-  game.load.image('ball2', 'assets/sprites/orb-red.png', 50, 50)
-  game.load.image('line', 'assets/sprites/line.PNG', 200, 4)
+  game.load.image('ball', 'assets/sprites/orb-blue.png')
+  game.load.image('ball2', 'assets/sprites/orb-red.png')
+  game.load.image('line', 'assets/sprites/line.PNG')
+  game.load.image('star', 'assets/sprites/star.png')
 
 }
 
@@ -20,6 +21,8 @@ var ledge
 var cursors
 var switchButton
 var killCount = 0
+var winText;
+var winSpot;
 
 
 function create(){
@@ -58,11 +61,20 @@ function create(){
 
   playerLedges = game.add.group()
 
+  winSpot = game.add.sprite(0, 100, 'star')
+
+  game.physics.arcade.enable(winSpot)
+
+  player.body.bounce.y = .2
+  player.body.gravity.y = 300
+  player.body.collideWorldBounds = true
+
 
 
 }
 
 function update(){
+  game.physics.arcade.overlap(player, winSpot, win, null, this);
 
   game.physics.arcade.collide(player, platforms)
   game.physics.arcade.collide(player, playerLedges)
@@ -73,6 +85,7 @@ function update(){
   if (switchButton.isDown && killCount === 0  ) {
 
    player.kill();
+
 
    px = player.body.position.x;
    py = player.body.position.y;
@@ -88,7 +101,7 @@ function update(){
    if (cursors.down.isDown && killCount === 1  ) {
      player.revive();
      player.body.position.x = 0;
-     player.body.position.y = 0;
+     player.body.position.y = game.world.height - 100;
      killCount = 0;
    }
 
@@ -107,4 +120,9 @@ function update(){
   {
      player.body.velocity.y = -350;
   }
+}
+
+function win () {
+  winText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000' });
+  winText.text = "you win!"
 }
