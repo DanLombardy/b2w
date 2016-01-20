3de39dc01b669a2b1dcfd878379480c57dbe2465
DanLombardy <lombardy.dan@gmail.com>
2016-01-20 15:37:47 -0800

    Add initial ninja conversion files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-var game = new Phaser.Game(800, 800, Phaser.AUTO, '',{
+var game = new Phaser.Game(800, 750, Phaser.AUTO, '',{
   preload: preload, create: create, update, update
-});
+})
 
 function preload(){
   game.load.image('ground', 'assets/sprites/platform.png')
   game.load.image('ball', 'assets/sprites/orb-blue.png', 50, 50)
   game.load.image('ball2', 'assets/sprites/orb-red.png', 50, 50)
   game.load.image('line', 'assets/sprites/line.PNG', 200, 4)
-
+  game.load.tilemap('map', 'assets/tilemaps/maps/ninja-tilemap.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('kenney', 'assets/tilemaps/tiles/kenney.png');
 }
 
 var playerLedges
@@ -16,95 +17,105 @@ var px
 var py
 var player
 var platforms
+var ground
 var ledge
 var cursors
 var switchButton
 var killCount = 0
-
+var map
+var tiles
+var layer
 
 function create(){
-
-  game.physics.startSystem(Phaser.Physics.ARCADE)
-
+//set up initial system environment
+  game.physics.startSystem(Phaser.Physics.NINJA)
   game.stage.backgroundColor = 0x4488cc
 
-  platforms = game.add.group()
+//add group for ground and standard ledges and config
+  platforms = game.add.group();
 
-  platforms.enableBody = true
+  ground = platforms.create(0, game.world.height - 64, 'ground');
+  ground.scale.setTo(2, 2);
 
-  var ground = platforms.create(0, game.world.height - 64, 'ground')
+  game.physics.ninja.enable(ground);
+  ground.body.immovable = true;
+  ground.body.gravityScale = 0;
 
-  ground.scale.setTo(2, 2)
+  ledge = platforms.create(400, 400, 'ground');
 
-  ground.body.immovable = true
+  game.physics.ninja.enable(ledge);
+  ledge.body.immovable = true;
+  ledge.body.gravityScale = 0;
 
-  ledge = platforms.create(400, 400, 'ground')
-  ledge.body.immovable = true
+  ledge = platforms.create(-150, 250, 'ground');
 
-  ledge = platforms.create(-150, 250, 'ground')
-	ledge.body.immovable = true
+  game.physics.ninja.enable(ledge);
+  ledge.body.immovable = true;
+  ledge.body.gravityScale = 0;
+//add player and initialize ninja physics on it
+  player = game.add.sprite(0, 20, 'ball')
+  game.physics.ninja.enable(player)
+  // game.physics.ninja.enableCircle(player,player.width/2)
 
 
-  player = game.add.sprite(32, game.world.height - 150, 'ball')
-
-  game.physics.arcade.enable(player)
-
-  player.body.bounce.y = .2
-  player.body.gravity.y = 300
-  player.body.collideWorldBounds = true
-
-  cursors = game.input.keyboard.createCursorKeys()
-  switchButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-
+  player.body.collideWorldBounds = true;
+  player.body.bounce = 0.2
+//game camera follows player
+  game.camera.follow(player)
+//add playerLedge group and capabilites
   playerLedges = game.add.group()
 
-
+  //add cursors
+  cursors = game.input.keyboard.createCursorKeys()
+  switchButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
 }
 
 function update(){
+//add collision for players, platforms, and playerLedges
+  game.physics.ninja.collide(player, platforms)
+  game.physics.ninja.collide(player, playerLedges)
 
-  game.physics.arcade.collide(player, platforms)
-  game.physics.arcade.collide(player, playerLedges)
 
-  player.body.velocity.x = 0
-
-  if (playerLedge) playerLedge.body.velocity.x = 0
-  if (switchButton.isDown && killCount === 0  ) {
-
-   player.kill();
-
-   px = player.body.position.x;
-   py = player.body.position.y;
-
-   // playerledge = game.add.sprite(px + i*60, py, 'playerledge');
-   playerLedge = playerLedges.create(px, py, 'line')
-   playerLedges.add(playerLedge);
-   game.physics.arcade.enable(playerLedge);
-   playerLedge.body.gravity.y = 0;
-   playerLedge.body.immovable = true;
-   killCount = 1;
-}
-   if (cursors.down.isDown && killCount === 1  ) {
-     player.revive();
-     player.body.position.x = 0;
-     player.body.position.y = 0;
-     killCount = 0;
-   }
-
+//add basic movement and jump to cursors
   if (cursors.left.isDown)
   {
-     player.body.velocity.x = -150;
+     player.body.moveLeft(150);
 
   }
   if (cursors.right.isDown)
   {
-     player.body.velocity.x = 150;
+     player.body.moveRight(150);
 
   }
 
   if (cursors.up.isDown && player.body.touching.down)
   {
-     player.body.velocity.y = -350;
+     player.body.moveUp(350)
   }
-}
+  if (switchButton.isDown && killCount === 0  ) {
+
+     player.kill();
+
+
+
+     px = player.body.x;
+     py = player.body.y;
+
+     // playerledge = game.add.sprite(px + i*60, py, 'playerledge');
+     playerLedge = playerLedges.create(px, py, 'line')
+     playerLedges.add(playerLedge);
+     game.physics.arcade.enable(playerLedge);
+     playerLedge.body.gravity.y = 0;
+     playerLedge.body.immovable = true;
+     killCount = 1;
+  }
+     if (cursors.down.isDown && killCount === 1  ) {
+       player.revive();
+       player.body.x = 0;
+       // player.body.position.y = game.world.height - 100;
+       player.body.y = 0;
+       killCount = 0;
+     }
+
+  }
