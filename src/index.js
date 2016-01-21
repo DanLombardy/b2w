cf3a8ca27b2dee5b6b0bbbe2b5433e3a015234c0
DanLombardy <lombardy.dan@gmail.com>
2016-01-20 19:03:19 -0800

    Add full ninja physics functionality and platform transfor functionality

--- src/index.js
+++ src/index.js
@@ -31,54 +31,68 @@ function create(){
   game.physics.startSystem(Phaser.Physics.NINJA)
   game.stage.backgroundColor = 0x4488cc
 
-//add group for ground and standard ledges and config
-  platforms = game.add.group();
+//add tilemap and tilemap physics
+  map = game.add.tilemap('map');
+  map.addTilesetImage('kenney');
+  layer = map.createLayer('Tile Layer 1');
+  layer.resizeWorld();
+  var slopeMap = { '32': 1, '77': 1, '95': 2, '36': 3, '137': 3, '140': 2 };
+  tiles = game.physics.ninja.convertTilemap(map, layer, slopeMap);
 
-  ground = platforms.create(0, game.world.height - 64, 'ground');
-  ground.scale.setTo(2, 2);
-
-  game.physics.ninja.enable(ground);
-  ground.body.immovable = true;
-  ground.body.gravityScale = 0;
-
-  ledge = platforms.create(400, 400, 'ground');
-
-  game.physics.ninja.enable(ledge);
-  ledge.body.immovable = true;
-  ledge.body.gravityScale = 0;
-
-  ledge = platforms.create(-150, 250, 'ground');
-
-  game.physics.ninja.enable(ledge);
-  ledge.body.immovable = true;
-  ledge.body.gravityScale = 0;
 //add player and initialize ninja physics on it
   player = game.add.sprite(0, 20, 'ball')
-  game.physics.ninja.enable(player)
-  // game.physics.ninja.enableCircle(player,player.width/2)
 
+  game.physics.ninja.enable(player)
 
   player.body.collideWorldBounds = true;
   player.body.bounce = 0.2
+
 //game camera follows player
   game.camera.follow(player)
-//add playerLedge group and capabilites
-  playerLedges = game.add.group()
 
-  //add cursors
+//add cursors
   cursors = game.input.keyboard.createCursorKeys()
   switchButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+
+  //add group for ground and standard ledges and config
+    platforms = game.add.group();
+
+
+
+    ledge = platforms.create(700, 400, 'ground');
+
+    game.physics.ninja.enable(ledge);
+    ledge.body.immovable = true;
+    ledge.body.gravityScale = 0;
+
+    ledge = platforms.create(-150, 250, 'ground');
+
+    game.physics.ninja.enable(ledge);
+    ledge.body.immovable = true;
+    ledge.body.gravityScale = 0;
+
+    //add playerLedge group and capabilites
+    playerLedges = game.add.group()
 
 }
 
 function update(){
-//add collision for players, platforms, and playerLedges
-  game.physics.ninja.collide(player, platforms)
-  game.physics.ninja.collide(player, playerLedges)
 
+  //add collision for players, platforms, and playerLedges
+    game.physics.ninja.collide(player, platforms)
+    game.physics.ninja.collide(player, playerLedges)
+
+
+  for (var i = 0; i < tiles.length; i++)
+    {
+      player.body.aabb.collideAABBVsTile(tiles[i].tile);
+    }
+
+    game.physics.ninja.collide(player, platforms)
+    game.physics.ninja.collide(player, playerLedges)
 
 //add basic movement and jump to cursors
-  if (cursors.left.isDown)
+  if (cursors.left.isDown )
   {
      player.body.moveLeft(150);
 
@@ -89,20 +103,18 @@ function update(){
 
   }
 
-  if (cursors.up.isDown && player.body.touching.down)
+  if (cursors.up.isDown && player.body.touching.down )
   {
      player.body.moveUp(350)
   }
+
   if (switchButton.isDown && killCount === 0  ) {
 
      player.kill();
 
-
-
      px = player.body.x;
      py = player.body.y;
 
-     // playerledge = game.add.sprite(px + i*60, py, 'playerledge');
      playerLedge = playerLedges.create(px, py, 'line')
      playerLedges.add(playerLedge);
      game.physics.ninja.enable(playerLedge);
@@ -111,12 +123,13 @@ function update(){
      playerLedge.body.immovable = true;
      killCount = 1;
   }
-     if (cursors.down.isDown && killCount === 1  ) {
-       player.revive();
-       player.body.x = 0;
-       // player.body.position.y = game.world.height - 100;
-       player.body.y = 0;
-       killCount = 0;
-     }
 
+  if (cursors.down.isDown && killCount === 1  ) {
+    player.revive();
+    player.body.x = px;
+    // player.body.position.y = game.world.height - 100;
+    player.body.y = py;
+    killCount = 0;
   }
+
+}
